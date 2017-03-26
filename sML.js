a9b062f41f6179dbc6ac1f820642cb4200238002
Satoru MATSUSHIMA (℠) <soarstre@gmail.com>
2017-03-26 19:56:18 +0900

    v0.999.44

--- sML.js
+++ sML.js
@@ -6,7 +6,7 @@
  * - Copyright (c) Satoru MATSUSHIMA - https://github.com/satorumurmur/sML
  * - Licensed under the MIT license. - http://www.opensource.org/licenses/mit-license.php
  *
- */ sML = (function() { var Version = "0.999.43", Build = 201703010218;
+ */ sML = (function() { var Version = "0.999.44", Build = 201703261955;
 
 
 
